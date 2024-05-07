3f361f6ee78dbc94bc9f798d9f7296bb326e7efb
AyushbLM10 <ayushbhattab65@gmail.com>
2024-05-07 19:38:17 +0530

    Home page completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
+import BalancedBox from '@/components/BalancedBox'
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: "Ayush"}
   return (
-    <div>page</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header>
+          <HeaderBox 
+            type = "greeting"
+            title = "Welcome"
+            user = {loggedIn?.firstName || "Guest"}
+            subtext = "Access and manage your account and transactions efficiently"
+            />
+          <BalancedBox 
+            accounts = {[]}
+            totalBanks = {1}
+            totalCurrentBalance = {1250.80}
+            />
+            
+        </header>
+      </div>
+    </section>
   )
 }
 
